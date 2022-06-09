4930387ee96cee3d979654e02aac31ee5501bfdc
cnfatal <cnfatal@gmail.com>
2022-06-09 21:42:13 +0800

    feat(template): support template dir
    Signed-off-by: cnfatal <cnfatal@gmail.com>

--- cmd/bundle/apps/download.go
+++ cmd/bundle/apps/download.go
@@ -5,12 +5,14 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/zapr"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	bundlev1 "kubegems.io/bundle-controller/pkg/apis/bundle/v1beta1"
 	"kubegems.io/bundle-controller/pkg/bundle"
@@ -62,13 +64,23 @@ func forBundleInPathes(pathes []string, fun func(*bundlev1.Bundle) error) error 
 	}
 
 	for _, path := range pathes {
-		content, err := os.ReadFile(path)
+		fi, err := os.Lstat(path)
 		if err != nil {
 			return err
 		}
-		objs, err := utils.SplitYAMLFilterd[*bundlev1.Bundle](bytes.NewReader(content))
-		if err != nil {
-			return err
+
+		var objs []*bundlev1.Bundle
+		if fi.IsDir() {
+			objs = []*bundlev1.Bundle{bundleFromDir(path)}
+		} else {
+			content, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			objs, err = utils.SplitYAMLFilterd[*bundlev1.Bundle](bytes.NewReader(content))
+			if err != nil {
+				return err
+			}
 		}
 		for _, obj := range objs {
 			if err := fun(obj); err != nil {
@@ -77,4 +89,25 @@ func forBundleInPathes(pathes []string, fun func(*bundlev1.Bundle) error) error 
 		}
 	}
 	return nil
+}
+
+func bundleFromDir(dir string) *bundlev1.Bundle {
+	// detect kind
+	kind := bundlev1.BundleKindTemplate
+	if _, err := os.Stat(filepath.Join(dir, "Chart.yaml")); err == nil {
+		kind = bundlev1.BundleKindHelm
+	} else if _, err := os.Stat(filepath.Join(dir, "kustomization.yaml")); err == nil {
+		kind = bundlev1.BundleKindKustomize
+	}
+	dir, _ = filepath.Abs(dir)
+	return &bundlev1.Bundle{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      filepath.Base(dir),
+			Namespace: "default",
+		},
+		Spec: bundlev1.BundleSpec{
+			Kind: kind,
+			URL:  "file://" + dir,
+		},
+	}
 }
